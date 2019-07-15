62219ab5f6ec6526febf00a900d0286ac62c627f
wyp <593704361@qq.com>
2019-07-15 13:51:46 +0800

    利用set查找数组是否有两个值和为指定数

---
+++ 利用set查找数组是否有任意两个数的和为指定数/index.js
@@ -0,0 +1,17 @@
+var arr1 = [3, 5, 6, 2, 62, 3, 4];
+var arr2 = [2, 5, 23, 2, 66];
+var num = 6
+
+
+function findNum(arr, num) {
+  let uniqueA = new Set();
+  uniqueA.add(num - arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if(uniqueA.has(arr[i])) return true
+    uniqueA.add(num - arr[i]);
+  }
+  return false
+}
+
+console.log(findNum(arr1, num))
+console.log(findNum(arr2, num))
